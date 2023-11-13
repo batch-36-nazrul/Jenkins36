9d95942d53007d652b6530be16a606e683a42fd9
studentqa2023 <rezwanislam857@gmail.com>
2023-11-12 20:12:40 -0500

    first commit

---
+++ src/main/java/com/jenkins_36/JenkinsGithub.java
@@ -0,0 +1,11 @@
+package com.jenkins_36;
+
+public class JenkinsGithub {
+	public void getJenkins() {
+		System.out.println("This is jenkins");
+	}
+public static void main(String[] args) {
+	JenkinsGithub obj = new JenkinsGithub();
+	obj.getJenkins();
+}
+}
